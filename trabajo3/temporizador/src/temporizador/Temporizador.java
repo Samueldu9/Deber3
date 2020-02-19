9500e6d2c42c17eadf4b2c1f35331a0979e94dd6
Samueldu9 <61156568+Samueldu9@users.noreply.github.com>
2020-02-19 15:33:08 -0500

    Add files via upload

---
+++ trabajo3/temporizador/src/temporizador/Temporizador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temporizador;
+
+/**
+ *
+ * @author samue
+ */
+public class Temporizador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmTemporizador frmTemporizador = new FrmTemporizador();
+        //frmAdivina.show();
+        frmTemporizador.setVisible(true);
+    }
+    
+}
